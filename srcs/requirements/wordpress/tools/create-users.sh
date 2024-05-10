afebe653a1e1a5f75314a90ee0a9630f4f8c63b3
MofMiq <marina.rodriguez1357@gmail.com>
2024-05-10 13:02:40 +0200

    ahora si que si

--- srcs/requirements/wordpress/tools/create-users.sh
+++ srcs/requirements/wordpress/tools/create-users.sh
@@ -6,13 +6,13 @@ wget -O /usr/local/bin/wp https://github.com/wp-cli/builds/raw/gh-pages/phar/wp-
 
 chmod +x /usr/local/bin/wp
 
-#wp core install --allow-root --url=$DOMAIN_NAME --title="Marirodr site" --admin_user=$DB_USER --admin_password=$DB_PASS --admin_email="marina.rodriguez1357@gmail.com" --path="/var/www/html"
+wp core install --allow-root --url=$DOMAIN_NAME --title="Marirodr site" --admin_user=$DB_USER --admin_password=$DB_PASS --admin_email="marina.rodriguez1357@gmail.com" --path="/var/www/html"
 
-#wp user create $WP_USER guest@example.com --role=author --user_pass=$WP_PASS --allow-root
+wp user create $WP_USER guest@example.com --role=author --user_pass=$WP_PASS --allow-root
 
-wp core install --allow-root --url=marirodr.42.fr --title="Marirodr site" --admin_user=marirodr --admin_password=wpass --admin_email="marina.rodriguez1357@gmail.com" --path="/var/www/html"
+#wp core install --allow-root --url=marirodr.42.fr --title="Marirodr site" --admin_user=marirodr --admin_password=wpass --admin_email="marina.rodriguez1357@gmail.com" --path="/var/www/html"
 
-wp user create guest guest@example.com --role=author --user_pass=1234 --allow-root
+#wp user create guest guest@example.com --role=author --user_pass=1234 --allow-root
 
 #wp option update siteurl $DOMAIN_NAME --allow-root --path=/var/www/html/
 #wp option update home $DOMAIN_NAME --allow-root --path=/var/www/html/
